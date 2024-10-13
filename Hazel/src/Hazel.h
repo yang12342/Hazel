6bbc463c434594645419105f80680863e7c85913
yang12342 <3211371632@qq.com>
2024-10-13 23:25:30 +0800

    Setup Applicatiion and Enter Point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// For use by Hazel application
+#include <stdio.h>
+#include  "Hazel/Application.h"
+
+
+//----Enter Point------------
+#include "Hazel/EnterPoint.h"
+//---------------------------
---
+++ Hazel/src/Hazel/Core.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+#ifdef  HZ_PLATFORM_WINDOWS
+	#ifdef HAZEL_BUILD_DLL
+		#define HAZEL_API __declspec(dllexport)
+	#else 
+		#define HAZEL_API __declspec(dllimport)
+	#endif // HZ_BUILD_DLL
+#else
+	#error Hazel only supports windows!
+
+#endif //  HZ_PLATFORM_WINDOWS
