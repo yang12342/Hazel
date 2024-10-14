07e5ba963770918f37129a998a1e850a013661e2
yang12342 <3211371632@qq.com>
2024-10-14 21:55:57 +0800

    log system and premake configuration

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,7 +3,7 @@
 // For use by Hazel application
 #include <stdio.h>
 #include  "Hazel/Application.h"
-
+#include "Hazel/Log.h"
 
 //----Enter Point------------
 #include "Hazel/EnterPoint.h"
--- Hazel/src/Hazel/EnterPoint.h
+++ Hazel/src/Hazel/EnterPoint.h
@@ -6,6 +6,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int  main(int argc,char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
+
+
 	printf("Hello Engine!");
 	auto app = Hazel::CreateApplication();
 	app->Run();
